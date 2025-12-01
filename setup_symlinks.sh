5d82fce43d68ba82e0357552b2d79a49e8dd0ebf
szymonolearczuk <szymon.olearczuk@fuseenergy.com>
2025-12-01 18:02:28 +0000

    aws config

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -4,3 +4,4 @@ ln -s $PWD/nvim $HOME/.config/nvim
 ln -s $PWD/home/gitconfig $HOME/.gitconfig
 ln -s $PWD/home/zshrc $HOME/.zshrc
 ln -s $PWD/home/tmux.conf $HOME/.tmux.conf
+ln -s $PWD/home/aws_config $HOME/.aws/config
